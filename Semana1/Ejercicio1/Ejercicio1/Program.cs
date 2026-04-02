f3aef34fa76fad3803cc937ae8fa17bd4ea9e863
br1an17 <brianzairpaez@gmail.com>
2026-04-02 17:51:13 -0300

    subimos ejercicio 1

--- Semana1/Ejercicio1/Ejercicio1/Program.cs
+++ Semana1/Ejercicio1/Ejercicio1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola");
+﻿//El precio de los productos no tiene incluido el incremento del 15%
+//que representa la ganancia del negocio, por lo tanto,
+//el algoritmo debe calcular el precio final a abonar por un cliente.
+//Se ingresa el precio del producto y como resultado muestra el precio de venta.
+
+using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        const double INCREMENTO = 0.15;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el precio del producto: ");
+
+            double precioProducto = Convert.ToDouble(Console.ReadLine());
+
+            double precioFinal = precioProducto + (precioProducto * INCREMENTO);
+
+            Console.WriteLine("El precio final a abonar por el cliente es: " + precioFinal);
+
+            // --- Agregue esta linea porque la consola luego de ingresar el precio se cerraba sola ---
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+       
+
+    }
+}
