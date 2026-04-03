183ee40221c9d504c48f22d68171bdbe0b8fcb65
br1an17 <brianzairpaez@gmail.com>
2026-04-03 15:14:50 -0300

    subimos ejercicio 5

---
+++ Semana1/Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,48 @@
+﻿//Resolver la consiga del punto 4 con la particularidad que el número puede
+//tener cualquier cantidad de dígitos, es decir,
+//puede ser un número de dos dígitos como un número de 6 dígitos.
+//Tomá la codificación anterior e introducí los cambios que sean necesarios.
+
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número");
+            string valor = Console.ReadLine();
+
+            int suma = 0;
+            string textoSuma = "";
+
+            while (!int.TryParse(valor, out _))
+            {
+                Console.WriteLine("El valor ingresado no es un número. Por favor, ingrese un número");
+                valor = Console.ReadLine();
+            }
+
+
+            for (int i = 0; i < valor.Length; i++)
+            {
+
+                int digito = Convert.ToInt32(valor.Substring(i, 1));
+
+
+                suma = suma + digito;
+
+
+                if (i < valor.Length - 1)
+                {
+                    textoSuma = textoSuma + digito + " + ";
+                }
+                else
+                {
+                    textoSuma = textoSuma + digito;
+                }
+            }
+            Console.WriteLine($"La suma de los digitos {textoSuma} es = {suma}");
+
+        }
+    }
+}
